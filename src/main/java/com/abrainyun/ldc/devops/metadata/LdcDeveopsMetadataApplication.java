d7be0f9f05b4f7cc2b96914072422dbf2bd75513
李琦 <liqi@abrain.com.cn>
2018-09-29 15:13:28 +0800

    first commit

---
+++ src/main/java/com/abrainyun/ldc/devops/metadata/LdcDeveopsMetadataApplication.java
@@ -0,0 +1,12 @@
+package com.abrainyun.ldc.devops.metadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdcDeveopsMetadataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdcDeveopsMetadataApplication.class, args);
+	}
+}
