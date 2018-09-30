a3dd87149a2f43aabda2327d38bd8e37af40fa83
姚金玉 <840973386@qq.com>
2018-09-30 17:51:47 +0800

    no message

--- src/main/java/com/abrainyun/ldc/devops/metadata/LdcDeveopsMetadataApplication.java
+++ src/main/java/com/abrainyun/ldc/devops/metadata/LdcDeveopsMetadataApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LdcDeveopsMetadataApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LdcDeveopsMetadataApplication.class, args);
+		    SpringApplication.run(LdcDeveopsMetadataApplication.class, args);
 	}
 }
